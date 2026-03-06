75c410d3b04e72c2669a9f53f6064008f629190d
Emanuel <92552607+Manolo789@users.noreply.github.com>
2026-03-06 11:19:57 -0300

    Update QKD_Extension.py
    QKD COW protocol added.

--- QKD_Extension.py
+++ QKD_Extension.py
@@ -10,6 +10,7 @@ from sequence.kernel.timeline import Timeline
 from sequence.qkd.BB84 import pair_bb84_protocols
 from sequence.qkd.B92 import pair_b92_protocols
 from sequence.topology.node import QKDNode
+from sequence.utils.encoding import time_bin
 import sequence.utils.log as log
 import numpy as np
 import pandas as pd
@@ -155,7 +156,70 @@ def simulation_B92(runtime=20, log_filename=-1, distance=1e3, polarization_fidel
 
 
 def simulation_COW():
-    pass
+    tl = Timeline(runtime*1e9)
+    tl.show_progress = True
+
+    # set log
+    if (log_filename != -1):
+        log.set_logger(__name__, tl, log_filename)
+        log.set_logger_level('DEBUG')
+        log.track_module('COW')
+        #log.track_module('timeline')
+        log.track_module('light_source')
+
+    qc0 = QuantumChannel("qc0", tl, distance=distance, polarization_fidelity=polarization_fidelity, attenuation=attenuation)
+    qc1 = QuantumChannel("qc1", tl, distance=distance, polarization_fidelity=polarization_fidelity, attenuation=attenuation)
+    cc0 = ClassicalChannel("cc0", tl, distance=distance)
+    cc1 = ClassicalChannel("cc1", tl, distance=distance)
+    cc0.delay += 1e9  # 1 ms
+    cc1.delay += 1e9
+
+    # Alice
+    ls_params = {"frequency": 80e6, "mean_photon_num": 0.1}
+    alice = QKDNode("alice", tl, encoding=time_bin, stack_size=1, qkdtype=2)
+    alice.set_seed(0)
+
+    for name, param in ls_params.items():
+        alice.update_lightsource_params(name, param)
+
+    # Bob
+    detector_params = [{"efficiency": 0.8, "dark_count": 10, "time_resolution": 10, "count_rate": 50e6},
+                       {"efficiency": 0.8, "dark_count": 10, "time_resolution": 10, "count_rate": 50e6}]
+    bob = QKDNode("bob", tl, encoding=time_bin, stack_size=1, qkdtype=2)
+    bob.set_seed(1)
+
+    for i in range(len(detector_params)):
+        for name, param in detector_params[i].items():
+            bob.update_detector_params(i, name, param)
+
+    qc0.set_ends(alice, bob.name)
+    qc1.set_ends(bob, alice.name)
+    cc0.set_ends(alice, bob.name)
+    cc1.set_ends(bob, alice.name)
+
+    # COW config
+    pair_cow_protocols(alice.protocol_stack[0], bob.protocol_stack[0])
+    process = Process(alice.protocol_stack[0], "push", [keysize, key_num, 6e12])
+    event = Event(0, process)
+    tl.schedule(event)
+
+    tl.init()
+    tl.run()
+
+    secret_key_rate_mean = 0
+
+    QBER = alice.protocol_stack[0].error_rates
+    THROUGHPUTS = alice.protocol_stack[0].throughputs
+    LATENCY = alice.protocol_stack[0].latency
+    for i, e in enumerate(alice.protocol_stack[0].error_rates):
+        R_s = alice.protocol_stack[0].sifted_bits_length[i]/len(alice.protocol_stack[0].bit_lists[0])
+        h_Q = binary_entropy(e)
+        R_sk = R_s*(1-h_Q)
+        secret_key_rate_mean += R_sk
+    SECRET_KEY_RATE = secret_key_rate_mean/len(alice.protocol_stack[0].error_rates)
+    LOSS = 1-10**((distance*attenuation)/(-10))
+    
+    return QBER, THROUGHPUTS, LATENCY, SECRET_KEY_RATE, LOSS
 
 # plot_graph (
 #            d_step = step of the distance (in meters),
@@ -176,6 +240,8 @@ def plot_graph(d_step, d_lim, att_lim, keysize):
     qber_list_bb84 = []
     skr_list_b92 = []
     qber_list_b92 = []
+    skr_list_cow = []
+    qber_list_cow = []
     d = 0
     while d <= d_lim:
         QBER, THROUGHPUTS, LATENCY, SECRET_KEY_RATE, LOSS = simulation_BB84(distance=d, attenuation=att_lim, keysize=keysize)
@@ -198,27 +264,26 @@ def plot_graph(d_step, d_lim, att_lim, keysize):
         d += d_step
     skr.append(skr_list_b92)
     qber.append(qber_list_b92)
-    '''
-    skr_list.clear()
-    qber_list.clear()
+
+
     d = 0
     while d <= d_lim:
         QBER, THROUGHPUTS, LATENCY, SECRET_KEY_RATE, LOSS = simulation_COW(distance=d, attenuation=att_lim, keysize=keysize)
-        skr_list.append(SECRET_KEY_RATE)
-        qber_list.append(np.mean(QBER))
+        skr_list_cow.append(SECRET_KEY_RATE)
+        qber_list_cow.append(np.mean(QBER))
         print()
         print(str((d/d_lim)*100)+'% concluído')
         d += d_step
-    skr.append(skr_list)
-    qber.append(qber_list)
-    '''
+    skr.append(skr_list_cow)
+    qber.append(qber_list_cow)
+
 
     # Convert skr, qber and d_list in numpy array
     skr_array = np.log10(np.array(skr))
     d_array = np.array(d_list)
     qber_array= np.array(qber)
 
-    log = {'R_sk - BB84': skr_array[0], "QBER - BB84": qber_array[0], 'R_sk - B92': skr_array[1], "QBER - B92": qber_array[1], 'distance': d_array}
+    log = {'R_sk - BB84': skr_array[0], "QBER - BB84": qber_array[0], 'R_sk - B92': skr_array[1], "QBER - B92": qber_array[1], 'R_sk - COW': skr_array[2], "QBER - COW": qber_array[2], 'distance': d_array}
     df = pd.DataFrame(log)
     df.to_csv('metrics.csv')
     
@@ -226,7 +291,7 @@ def plot_graph(d_step, d_lim, att_lim, keysize):
     fig, ax1 = plt.subplots()
     linha_y1, = ax1.plot(d_array, skr_array[0], linestyle='-', color='blue', label="R_sk(d) of the BB84")
     linha_y2, = ax1.plot(d_array, skr_array[1], linestyle='-', color='red', label="R_sk(d) of the B92")
-#    linha_y3, = ax1.plot(d_array, skr_array[2], linestyle='-', color='green', label="R_sk(d) of the COW")
+    linha_y3, = ax1.plot(d_array, skr_array[2], linestyle='-', color='green', label="R_sk(d) of the COW")
     ax1.set_xlabel("Distance (d) [m]")
     ax1.set_ylabel("Log_10 Secret Key Rate (R_sk) [bits per sent qubit]")
     ax1.set_title("Attenuation [dB/m]:"+str(att_lim)+", Keysize [bits of width]:"+str(keysize))
@@ -234,11 +299,10 @@ def plot_graph(d_step, d_lim, att_lim, keysize):
     ax2 = ax1.twinx()
     linha_z1, = ax2.plot(d_array, qber_array[0], linestyle='--', color='orange', label="QBER(d) of the BB84")
     linha_z2, = ax2.plot(d_array, qber_array[1], linestyle='--', color='yellow', label="QBER(d) of the B92")
-#    linha_z3, = ax2.plot(d_array, qber_array[2], linestyle='--', color='black', label="QBER(d) of the COW")
+    linha_z3, = ax2.plot(d_array, qber_array[2], linestyle='--', color='black', label="QBER(d) of the COW")
     ax2.set_ylabel("QBER")
 
-#    linhas = [linha_y1, linha_y2, linha_y3, linha_z1, linha_z2, linha_z3]
-    linhas = [linha_y1, linha_y2, linha_z1, linha_z2]
+    linhas = [linha_y1, linha_y2, linha_y3, linha_z1, linha_z2, linha_z3]
     labels = [l.get_label() for l in linhas]
     ax1.legend(linhas, labels, loc="best")
 
@@ -246,14 +310,11 @@ def plot_graph(d_step, d_lim, att_lim, keysize):
     plt.show()
 
 def run_simulation():
-    # constants
-    #log_filename = "bb84.log"
-    #interactive_plot = interact(simulation_BB84, runtime=(1, 10, 20), attenuation=(0.0002, 0.002, 0.02), keysize=[128, 256, 512])
-    #interactive_plot
     plot_graph(100, 10000, 0.0002, 25)
 
 if __name__ == "__main__":
     run_simulation()
+
 
 
 
