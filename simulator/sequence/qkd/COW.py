6a35a32de65c746f02bd3959915573ffeac28c21
Emanuel <92552607+Manolo789@users.noreply.github.com>
2026-03-09 14:50:19 -0300

    Update COW.py

--- simulator/sequence/qkd/COW.py
+++ simulator/sequence/qkd/COW.py
@@ -17,7 +17,7 @@ modification, are permitted provided that the following conditions are met:
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
 
-    * Neither the name AsmMathLib nor the names of any AsmMathLib contributors
+    * Neither the name SeQUeNCe-QKDprotocols nor the names of any SeQUeNCe-QKDprotocols contributors
       may be used to endorse or promote products derived from this software
       without specific prior written permission.
 
